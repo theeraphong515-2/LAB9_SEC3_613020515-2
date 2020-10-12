a20a4e2d87c9fbd507a10d91cb41203b8e8f2dd6
Theeraphong <theeraphong.th@kkumail.com>
2020-10-12 23:44:54 +0700

    ส่งLAB9ครับ

---
+++ app/src/main/java/com/myweb/lab9mysqlupdatedelete/EditDeleteActivity.kt
@@ -0,0 +1,11 @@
+package com.myweb.lab9mysqlupdatedelete
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_delete)
+    }
+}
